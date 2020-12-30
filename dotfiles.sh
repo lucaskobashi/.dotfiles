b947ba093a8e2a5c0a8ec69641f978b1ae709d33
lucas kobashi <lucas.kobashi@cs.usask.ca>
2020-12-30 08:54:29 -0300

    added shell script

---
+++ dotfiles.sh
@@ -0,0 +1,25 @@
+# checks if directory was cloned successfully
+if [ ! -d ~/.dotfiles/ ]
+then
+    echo "cloning directory from github"
+    git clone https://github.com/lucaskobashi/.dotfiles.git ~
+fi
+
+# changes directory to .dotfiles
+echo "changing directory to ~/.dotfiles"
+cd ~/.dotfiles
+
+# deletes .DS_Store files that might have been downloaded
+echo "deleting .DS_Store files from ~/.dotfiles"
+find . -type f -name '*.DS_Store' -ls -delete
+
+echo "create symlink? (yes or no)"
+read CHOICE
+
+if [ $CHOICE = "y" -o $CHOICE = "yes" ]
+then
+    echo "creating symlink."
+    stow -vt ~ git bash htop tmux vim
+else
+    echo "symlink not created."
+fi
